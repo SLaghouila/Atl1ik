976f2084d72d2a32688915abe9bd25de8f4d04a0
SineVJensen <sine144@gmail.com>
2013-04-08 14:11:56 +0200

    Uden funktioner, men med style som baneturnering.dk

--- adminforside.php
+++ adminforside.php
@@ -1,17 +1,42 @@
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
 
-<head>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Administrator</title>
-</head>
+
+<link rel="stylesheet" href="http://www.baneturneringen.dk/wp-content/themes/default/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="http://www.baneturneringen.dk/wp-content/themes/default/style.css" type="text/css" media="screen" />
+<link rel="pingback" href="http://www.baneturneringen.dk/xmlrpc.php" />
+
+
+
+
+<link rel="alternate" type="application/rss+xml" title="Baneturneringen &raquo; Hjem Comments Feed" href="http://www.baneturneringen.dk/hjem-2/feed/" />
+<script type='text/javascript' src='http://www.baneturneringen.dk/wp-includes/js/jquery/jquery.js?ver=1.4.2'></script>
+<script type='text/javascript' src='http://www.baneturneringen.dk/wp-includes/js/comment-reply.js?ver=20090102'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.baneturneringen.dk/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.baneturneringen.dk/wp-includes/wlwmanifest.xml" /> 
+<link rel='index' title='Baneturneringen' href='http://www.baneturneringen.dk/' />
+<link rel='prev' title='21.-22. august' href='http://www.baneturneringen.dk/resultater/21-22-august/' />
+<link rel='next' title='Runder 2012' href='http://www.baneturneringen.dk/runder2012/runder-2012/' />
+<meta name="generator" content="WordPress 3.0" />
+<link rel='canonical' href='http://www.baneturneringen.dk/' />
+<style type="text/css" media="all">
+/* <![CDATA[ */
+@import url("http://www.baneturneringen.dk/wp-content/plugins/wp-table-reloaded/css/plugin.css?ver=1.7");
+@import url("http://www.baneturneringen.dk/wp-content/plugins/wp-table-reloaded/css/datatables.css?ver=1.7");
+/* ]]> */
+</style></head>
 
 <body>
-<h1>Administrator</h1>
-	Velkommen
+<center><h1>Administrator</h1></center>
+<center>Velkommen
 <br>
-<input type="button" value="Hent deltageroplysninger"> <br>
-<input type="button" value="Rediger deltageroplysninger"> <br>
-<input type="button" value="rediger runder"> <br>
-<input type="button" value="Start ny baneturnering">
+<input type="button" value="Hent deltageroplysninger" style="width:190px;"> <br>
+<input type="button" value="Rediger deltageroplysninger" style="width:190px;"> <br>
+<input type="button" value="rediger runder" style="width:190px;"> <br>
+<input type="button" value="Start ny baneturnering" style="width:190px;"></center>
 
 
 </body>
