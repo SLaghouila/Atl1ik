bad229d01be32b4b1c85c7f4612207ee99ef43cb
SineVJensen <sine144@gmail.com>
2013-04-08 11:57:21 +0200

    Skelettet er lavet, men langt fra pænt

---
+++ adminforside.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<title>Administrator</title>
+</head>
+
+<body>
+<h1>Administrator</h1>
+	Velkommen
+<br>
+<input type="button" value="Hent deltageroplysninger"> <br>
+<input type="button" value="Rediger deltageroplysninger"> <br>
+<input type="button" value="rediger runder"> <br>
+<input type="button" value="Start ny baneturnering">
+
+
+</body>
+
+		
+
+</html>
